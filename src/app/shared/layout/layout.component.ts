da0ec587517dff8b4b393d0f5f25612c1d2bebc6
Mehdi Rochereau <271727509+mehdi-rochereau@users.noreply.github.com>
2026-04-23 16:04:13 +0200

    feat: add navigation link on TaskFlow brand logo

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../core/services/auth.service';
@@ -7,7 +7,7 @@ import { AuthService } from '../../core/services/auth.service';
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
